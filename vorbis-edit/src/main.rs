f374b6fb9b9bfd0f64b758fcb920e0ea4f5828ff
Sam Price <sam@typicalsamprice.com>
2023-07-23 01:21:42 -0500

    Fix bugs in vorbis-edit
    Files with '.' in their name won't crash this thing
        e.g. "Mr. Blue Sky" by the Electric Light Orchestra
    
    Naming conventions:
    If the artist/track title has a slash in the name, use a space
    or other replacement character, but the metadata should have the proper
    name.
    
    For featuring artists, the metadata should specify multiple 'artist'
    tags, but 'PRIMARY ARTIST - TRACK TITLE (feat. FEATURED ARTIST)' is the
    file naming style.
    
    Usually, names should be stylized as according to published (by the
    artist[s]) records.
        i.e. JVKE and his song 'golden hour', even though it "properly" is
        written as "Golden Hour by Jvke"

--- vorbis-edit/src/main.rs
+++ vorbis-edit/src/main.rs
@@ -18,20 +18,20 @@ fn main() {
 
 fn vorbis_edit_file(filename: &str) {
     let mut file_name = filename.to_owned();
-    let (fname, ext) = file_name
-        .split_once(".")
-        .expect("vorbis-edit called with file that has no extension");
-    assert_eq!(ext, "ogg");
+    assert!(file_name.len() > 4); // Must be more than ".ogg"
+    let fname = (&file_name[0..file_name.len() - 4]).to_owned();
+    let ext = &file_name[file_name.len() - 4..];
+    assert_eq!(ext, ".ogg");
 
     let _ = File::open(&file_name).expect("vorbis-edit called with nonexistent file");
 
     let (purported_filename, vorbis) = if let Some((artist, title)) = fname.split_once(" - ") {
-        (fname, Some((artist.to_owned(), title.to_owned())))
+        (fname.clone(), Some((artist.to_owned(), title.to_owned())))
     } else {
-        (fname, None)
+        (fname.clone(), None)
     };
 
-    let (mut artist, mut title) = vorbis.unwrap_or(("unknown".to_owned(), "unknown".to_owned()));
+    let (mut artist, mut title) = vorbis.unwrap_or((fname.clone(), fname.clone()));
     print!(
         "Would you like to change the filename?\nCurrent filename: {}\nCurrent artist: {}\nCurrent title: {}\n[y/N]: ",
         purported_filename, artist, title
@@ -43,44 +43,33 @@ fn vorbis_edit_file(filename: &str) {
         0
     });
 
-    let rel_bytes = &should_edit.as_bytes()[0..3.min(should_edit.len())];
+    let rel_bytes = should_edit.as_bytes()[0];
     let should_edit = match rel_bytes {
-        b"yes" => true,
-        b"ye" => true,
-        b"y" => true,
+        b'y' => true,
         _ => false,
     };
 
     if should_edit {
         let mut n_artist = String::new();
         let mut n_title = String::new();
-        print!("Enter artist: ");
-        io::stdout().flush().unwrap();
-        io::stdin()
-            .read_line(&mut n_artist)
-            .expect("did not give an artist");
 
-        print!("Enter title: ");
-        io::stdout().flush().unwrap();
-        io::stdin()
-            .read_line(&mut n_title)
-            .expect("did not give a title");
+        get_user_input_or_default("Enter artist", &mut n_artist, &artist);
+        get_user_input_or_default("Enter title", &mut n_title, &title);
 
         artist = n_artist.trim_end().to_owned().clone();
         title = n_title.trim_end().to_owned().clone();
 
         let new_filename = format!("{artist} - {title}.ogg");
-        fs::rename(&file_name, &new_filename).expect("failed to rename file");
+        let Ok(()) = fs::rename(&file_name, &new_filename) else {
+            panic!("failed to rename '{file_name}' to '{new_filename}'");
+        };
         println!("Renamed '{}' to '{}'", &file_name, &new_filename);
 
         file_name = new_filename.clone();
     }
 
+    // Just make sure any "mv" works
     let _ = File::open(&file_name).expect("could not open file to verify it is real");
-    // Just make sure the tempfile is clear
-    Command::new("rm").arg("-f").arg(TEMPFILE).output().unwrap();
-    File::create(TEMPFILE).unwrap();
-
     let boilerplate = format!(
         "{}\nartist={}\ntitle={}\n{}",
         "language=eng\nencoder=Lavc60.3.100 libvorbis", artist, title, "genre=\nalbum=\ndate=\n"
@@ -88,16 +77,33 @@ fn vorbis_edit_file(filename: &str) {
 
     fs::write(TEMPFILE, boilerplate).unwrap();
 
-    let mut nvim = Command::new("sh");
-    nvim.arg("-c").arg(format!("nvim {TEMPFILE}").as_str());
-    nvim.spawn().unwrap().wait().unwrap();
+    edit_vorbis_tempfile(TEMPFILE);
+    set_vorbis_metadata(&file_name, TEMPFILE);
+}
 
-    let mut vorbiscomment = Command::new("vorbiscomment");
-    vorbiscomment
-        .arg("-c")
-        .arg(TEMPFILE)
-        .arg("-w")
-        .arg(&file_name);
-    vorbiscomment.output().unwrap();
-    Command::new("rm").arg("-f").arg(TEMPFILE).output().unwrap();
+fn get_user_input_or_default(prompt: &str, place_into: &mut String, default: &str) {
+    let printed = format!("{prompt} (default: '{default}'): ");
+    print!("{}", printed);
+    io::stdout().flush().unwrap();
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).unwrap();
+    let buf_nice = buf.trim_end();
+    if buf_nice.len() == 0 {
+        default.clone_into(place_into);
+    } else {
+        buf_nice.clone_into(place_into);
+    }
+}
+
+fn edit_vorbis_tempfile(file: &str) {
+    let mut sh = Command::new("sh");
+    sh.arg("-c").arg(format!("nvim {file}").as_str());
+    sh.spawn().unwrap().wait().unwrap();
+}
+fn set_vorbis_metadata(ogg_file: &str, tempfile: &str) {
+    let mut vc = Command::new("vorbiscomment");
+    vc.arg("-c").arg(tempfile).arg("-w").arg(ogg_file);
+    vc.output().unwrap();
+    // Clean up tempfile
+    Command::new("rm").arg("-f").arg(tempfile).output().unwrap();
 }
